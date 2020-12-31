efae455a924d7350228981df62e6fd968c7211fa
Kimbyeongmin <Kimbyeongmin@59.22.8.25>
2020-12-31 11:27:56 +0900

    git.test.java 수정

--- src/GitTest.java
+++ src/GitTest.java
@@ -3,7 +3,8 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("git test");
+		System.out.println("git test 1");
+		System.out.println("git test 2");
 	}
 
 }
