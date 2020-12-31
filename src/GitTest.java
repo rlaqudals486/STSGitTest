66fe8be2461010b55fe72f0b6dbbd0c5aef25e70
Kimbyeongmin <Kimbyeongmin@59.22.8.25>
2020-12-31 12:31:13 +0900

    git에서 다운받아 수정

--- src/GitTest.java
+++ src/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 		// TODO Auto-generated method stub
 		System.out.println("git test 1");
 		System.out.println("git test 2");
+		System.out.println("git 에서 다운받아 수정한 파일");
 	}
 
 }
